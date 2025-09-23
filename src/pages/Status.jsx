d40913577e834020a43a96cd604a18b4111531f6
abhirup2565 <abhirup2565@gmail.com>
2025-09-23 17:51:12 +0530

    status api call

--- src/pages/Status.jsx
+++ src/pages/Status.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useAppContext } from "../wrappers/AppContext";
+//import { useAppContext } from "../wrappers/AppContext";
 import { handleRefresh} from "../utils/handleRefresh";
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,10 +9,23 @@ import { FileText } from "lucide-react";
 import { FaDownload, FaLink } from "react-icons/fa";
 import "../styles/Status.css"
 import Error from "../components/Error";
+import { statusList } from "../networks/statusList";
 
 const Status = () => {
-  const { signatures, setSignatures } = useAppContext();
+  const [ signatures, setSignatures ] = useState([]);
   const [errors, setErrors] = useState([]);
+
+  useEffect(() => {
+   const fetch = async () => {
+    try {
+      const data = await statusList(setErrors);
+      setSignatures(data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+    };
+    fetch()
+    },[setErrors]);
 
 useEffect(() => {
     signatures.forEach(signatureRecord => {
@@ -23,13 +36,13 @@ useEffect(() => {
         // Mark the signature as polling to avoid multiple triggers
         setSignatures(prev =>
           prev.map(sig =>
-            sig.signatureId === signatureRecord.signatureId
+            sig.signature_id === signatureRecord.signature_id
               ? { ...sig, polling: true }
               : sig
           )
         );
 
-        handleRefresh(signatureRecord.signatureId, setSignatures, setErrors);
+        handleRefresh(signatureRecord.signature_id, setSignatures, setErrors);
       }
     });
     console.log(signatures);
@@ -88,14 +101,14 @@ const StatusTable = ({ signatures, setErrors }) => {
                 <tr key={`${signatureRecord.signatureId}-${index}`}>
                   {index === 0 && (
                     <td rowSpan={signerCount} className="doc-id">
-                      {signatureRecord.documentId}
+                      {signatureRecord.document_id}
                     </td>
                   )}
-                  <td>{signer.name}</td>
+                  <td>{signer.username}</td>
                   <td>{getStatusBadge(signer.status)}</td>
                   <td>
                     <a
-                      href={signer.signatureUrl}
+                      href={signer.signer_url}
                       target="_blank"
                       rel="noopener noreferrer"
                       className="link-icon"
